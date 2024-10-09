92b0f8f4e439af76e5b1a9272e1a42695058a1a7
Hardik Pratap <hardikpratap188@gmail.com>
2024-10-10 00:53:43 +0530

    Local Storage implemented

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 import TaskForm from '../components/TaskForm'
 import TaskCol from '../components/TaskCol'
@@ -6,10 +6,17 @@ import todoIcon from '../assets/direct-hit.png'
 import doingIcon from'../assets/glowing-star.png'
 import doneIcon from'../assets/check-mark-button.png'
 
+const oldTasks=localStorage.getItem("tasks");
+console.log(oldTasks);
+
 
 function App() {
 
-  const [tasks, setTasks]= useState([]);
+  const [tasks, setTasks]= useState(JSON.parse(oldTasks));
+
+  useEffect(() => {localStorage.setItem("tasks", JSON.stringify(tasks))
+  },[tasks]);
+
 
   function handleDelete(taskIndex){
     const newTasks= tasks.filter((task,index )=>index !==taskIndex);
